4808a3bb895f3ba0322a26c4ee1ad9d864a726ff
drili <DriliB@hotmail.com>
2023-06-13 11:43:48 +0200

    :construction: templates

---
+++ parts/footer.php
@@ -0,0 +1,22 @@
+        <script>
+            console.log("::: MSG: footer.php: Footer initiated")
+        </script>
+
+        <!-- *** AOS -->
+        <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+        <script>
+            AOS.init();
+        </script>
+
+        <!-- *** jQuery Toast -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-toast-plugin/1.3.2/jquery.toast.min.js" integrity="sha512-zlWWyZq71UMApAjih4WkaRpikgY9Bz1oXIW5G0fED4vk14JjGlQ1UmkGM392jEULP8jbNMiwLWdM8Z87Hu88Fw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+        <!-- *** Select2 -->
+        <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+
+        <!-- *** Quill library -->
+        <script src="https://cdn.quilljs.com/1.3.6/quill.js"></script>
+
+        <script src="<?php echo $relative_directory; ?>/__js/global.js"></script>
+    </body>
+</html>
