b1a29ec326d54261b4e9c67cb702f7fb8c8fd5bc
drili <DriliB@hotmail.com>
2023-06-16 10:29:24 +0200

    :hammer: sidebar links active checker

--- __js/global.js
+++ __js/global.js
@@ -1,1 +1,12 @@
-console.log(`::: global.js`);
+document.addEventListener("DOMContentLoaded", function() {
+    // *** Sidebar active link checker
+    (function sideBarActiveLink() {
+        var currentPageUrl = window.location.href;
+        var menuLinks = document.querySelectorAll(".link-section a");
+        menuLinks.forEach(function(link) {
+            if (link.href === currentPageUrl) {
+                link.classList.add("link-active");
+            }
+        })
+    })();
+})
--- components/page_layout/Sidebar.php
+++ components/page_layout/Sidebar.php
@@ -9,7 +9,7 @@
     <div class="sidebar-main-content sidebar-section">
         <h6>Main Tools</h6>
         <div class="link-section">
-            <a href="<?php echo $relative_directory; ?>/views/dashboard.php" class="link-active"><span class="icon"><i class="fa fa-dashboard"></i></span> Dashboard</a>
+            <a href="<?php echo $relative_directory; ?>/views/dashboard.php"><span class="icon"><i class="fa fa-dashboard"></i></span> Dashboard</a>
             <a href="<?php echo $relative_directory; ?>/views/workflow.php"><span class="icon"><i class="fa fa-columns"></i></span> Workflow</a>
             <a href="<?php echo $relative_directory; ?>/views/sprint-overview.php"><span class="icon"><i class="fa fa-calendar"></i></span> Sprint Overview</a>
             <a href="<?php echo $relative_directory; ?>/views/time-registrations.php"><span class="icon"><i class="fa fa-clock"></i></span> Time Registrations</a>
